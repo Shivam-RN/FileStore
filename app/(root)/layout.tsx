063eee889075fb73d1c61b533cee9b262571304e
Shivam-RN <shivamrana1809@gmail.com>
2025-06-23 20:08:28 +0530

    signup and signin

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const dynamic = "force-dynamic";
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+
+  return (
+    <main className="flex h-screen"> 
+    </main>
+  );
+};
+export default Layout;
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl text-brand">hello</h1>
+    </div>
+  );
+}
